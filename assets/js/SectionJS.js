092975c8e6311cc480d02e4d6020892e093b7e0c
Danny <thanhan1100@gmail.com>
2023-12-08 20:59:13 +0700

    Fixes
    - not really, idk what im fixed :)

--- assets/js/SectionJS.js
+++ assets/js/SectionJS.js
@@ -2,3 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     var video = document.getElementById('backgroundVideo');
     video.play();
   });
+
+// TODO: RANDOM VIDEO ON LOADING PAGE IN THE BACKGROUND
+// List of video URLs
+const videoList = [
+  '/assets/vid/GTA_VI_trailer.mp4',
+  '/assets/vid/TUNIC_trailer.mp4',
+  // Add more video URLs here
+];
+
+// Function to select a random video URL
+function getRandomVideo() {
+  const randomIndex = Math.floor(Math.random() * videoList.length);
+  return videoList[randomIndex];
+}
+
+// Get video element
+const backgroundVideo = document.getElementById('backgroundVideo');
+
+// Set a random video as the source
+backgroundVideo.querySelector('source').src = getRandomVideo();
+
+// Reload the video to apply the new source
+backgroundVideo.load();
+
