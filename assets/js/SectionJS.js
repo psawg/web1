059dff890a3caacba7d8f29c98b5139cccb12c34
sang <psg.exorcist@gmail.com>
2023-12-10 16:06:52 +0700

    UPDATE SECTION CONTENT

--- assets/js/SectionJS.js
+++ assets/js/SectionJS.js
@@ -30,12 +30,31 @@
       newsItems.forEach(function(item) {
         item.addEventListener("mouseenter", function() {
           isHovered = true;
-          this.style.transform = "scale(1.1)"; // Scale lên 1.1 khi di chuột vào
+          this.style.transform = "scale(1.2)"; // Scale lên 1.1 khi di chuột vào
+          this.style.transition = "transform 1s ease"; // Thêm transition cho hiệu ứng mượt hơn
+          this.style.zIndex = "2"; // Đặt giá trị z-index cao hơn để nổi lên trên các thành phần khác
+
+
+          // Thay đổi margin của các thành phần xung quanh
+      var siblings = getSiblings(this);
+      siblings.forEach(function(sibling) {
+        // sibling.style.marginLeft = "10px"; // Di chuyển sang trái
+        sibling.style.marginRight = "10px"; // Di chuyển sang phải
+      });
         });
     
         item.addEventListener("mouseleave", function() {
           isHovered = false;
           this.style.transform = "scale(1)"; // Scale về 1 khi di chuột ra
+          this.style.transition = "transform 1s ease"; // Thêm transition cho hiệu ứng mượt hơn
+          this.style.zIndex = "0"; // Đặt lại giá trị z-index ban đầu
+
+           // Đặt lại margin của các thành phần xung quanh về giá trị ban đầu
+      var siblings = getSiblings(this);
+      siblings.forEach(function(sibling) {
+        sibling.style.marginLeft = "0"; // Đặt lại margin trái về giá trị ban đầu
+        sibling.style.marginRight = "0"; // Đặt lại margin phải về giá trị ban đầu
+      });
         });
       });
     
@@ -48,11 +67,27 @@
           // Di chuyển tin tức đầu tiên đến cuối danh sách
           firstItem.style.transform = "translateX(-100%)";
     
+        
           setTimeout(function() {
             // Đặt tin tức đó vào cuối danh sách
-            newsList.appendChild(firstItem);
             firstItem.style.transform = "translateX(0)";
+            newsList.appendChild(firstItem);
           }, 1000); // Thời gian chuyển động (ms)
         }
       }, 3000); // Thời gian chờ giữa các chuyển động (ms)
     };
+
+
+
+    // Hàm lấy các sibling của một element
+function getSiblings(element) {
+  var siblings = [];
+  var sibling = element.parentNode.firstChild;
+  while (sibling) {
+    if (sibling.nodeType === 1 && sibling !== element) {
+      siblings.push(sibling);
+    }
+    sibling = sibling.nextSibling;
+  }
+  return siblings;
+}
