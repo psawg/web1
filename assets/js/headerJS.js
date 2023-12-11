2899a90972aa6ec065eeeb627e25d81755dd44ac
Danny <thanhan1100@gmail.com>
2023-12-11 16:16:32 +0700

    Header Update
    - Responsive Hamburger Navbar (NOT DONE)

---
+++ assets/js/headerJS.js
@@ -0,0 +1,10 @@
+// Hamburger Open Side Nav
+function openNav() {
+    document.getElementById("idSideNav").style.width = "250px";
+}
+
+// Hamburger Close Side Nav
+function closeNav() {
+    document.getElementById("idSideNav").style.width = "0px";
+
+}
