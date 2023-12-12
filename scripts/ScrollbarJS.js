46083760519a599cb8f29bb9e2bfb6c223dbc97f
sang <psg.exorcist@gmail.com>
2023-12-12 16:16:19 +0700

    UPDATE Scrollbar

--- scripts/ScrollbarJS.js
+++ scripts/ScrollbarJS.js
@@ -1,7 +1,18 @@
-window.addEventListener('DOMContentLoaded', function() {
-    // Lấy phần tử body của trang
-    var body = document.querySelector('body');
-    
-    // Thêm lớp CSS "scrollbar" vào phần tử body
-    body.classList.add('scrollbar');
+
+window.addEventListener("DOMContentLoaded", function () {
+  var body = document.querySelector("body");
+
+  body.classList.add("scrollbar");
+
+  // Thêm sự kiện khi di chuột vào thanh cuộn
+  body.addEventListener("mouseover", function () {
+    // body.classList.add("scrollbar1");
+    body.classList.remove("scrollbar1");
   });
+
+  // Thêm sự kiện khi di chuột ra khỏi thanh cuộn
+  body.addEventListener("mouseout", function () {
+    body.classList.add("scrollbar1");
+    // body.classList.remove("scrollbar1");
+  });
+});
