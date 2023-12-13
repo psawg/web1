5684f473fd25d35be1022e7194cbf0a72071ab1b
sang <psg.exorcist@gmail.com>
2023-12-14 00:36:28 +0700

    lấy về rồi làm nha mng

--- scripts/ScrollbarJS.js
+++ scripts/ScrollbarJS.js
@@ -1,18 +1,6 @@
-
 window.addEventListener("DOMContentLoaded", function () {
   var body = document.querySelector("body");
-
   body.classList.add("scrollbar");
-
-  // Thêm sự kiện khi di chuột vào thanh cuộn
-  body.addEventListener("mouseover", function () {
-    // body.classList.add("scrollbar1");
-    body.classList.remove("scrollbar1");
-  });
-
-  // Thêm sự kiện khi di chuột ra khỏi thanh cuộn
-  body.addEventListener("mouseout", function () {
-    body.classList.add("scrollbar1");
-    // body.classList.remove("scrollbar1");
-  });
 });
+
+
