1650752754740073173718fe2a0fa283f49b7fd4
zeno1406 <ngthanhlong2003@gmail.com>
2023-12-14 16:49:27 +0700

    create scroll to top button

--- scripts/Minecraft_PageJS.js
+++ scripts/Minecraft_PageJS.js
@@ -1,28 +1,43 @@
-  let slideIndex = 1;
-  showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
 
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+//cuộn xuống 200px tính từ đầu trang thì sẽ hiện button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
   }
+}
+//nhấp vào button sẽ cuộn lên trang đầu
+function scrollToTop() {
+window.scrollTo({ top: 0, behavior: "smooth" });
+};
